2693d2fad898afbc78d4008b7215744a4b6f9471
adey-px <aosteph@gmail.com>
2023-08-04 20:03:41 -0500

    Home: added bootstrap for better ux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,60 @@
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { products } from './datasource/data';
 
 function App() {
 	return (
-		<>
-			<header>Typsco</header>
+		<div className='d-flex flex-column vh-100'>
+			<header>
+				<Navbar>
+					<Container>
+						<Navbar.Brand>Typsco</Navbar.Brand>
+					</Container>
+					<Nav>
+						<a
+							href='/cart'
+							className='nav-link'
+						>
+							Cart
+						</a>
+						<a
+							href='/signin'
+							className='nav-link'
+						>
+							Login
+						</a>
+					</Nav>
+				</Navbar>
+			</header>
 
 			<main>
-				<ul>
-					{products.map((product) => (
-						<li key={product.slug}>
-							<img
-								src={product.image}
-								alt={product.name}
-								className='product-img'
-							/>
-							<h2>{product.name}</h2>
-							<p>${product.price}</p>
-						</li>
-					))}
-				</ul>
+				<Container className='mt-3'>
+					<Row>
+						{products.map((product) => (
+							<Col
+								key={product.slug}
+								sm={6}
+								md={4}
+								lg={3}
+							>
+								<img
+									src={product.image}
+									alt={product.name}
+									className='product-img'
+								/>
+								<h4>{product.name}</h4>
+								<p>${product.price}</p>
+							</Col>
+						))}
+					</Row>
+				</Container>
 			</main>
 
 			<footer>
-				<span>&#169;&#65039;All Rights Reserved</span>
+				<div className='text-center'>
+					<span>&#169;&#65039;All Rights Reserved</span>
+				</div>
 			</footer>
-		</>
+		</div>
 	);
 }
 
